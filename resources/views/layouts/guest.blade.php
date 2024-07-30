6449d6f361d6c9c4ada6116eee8c6e8b135b7040
ilzam <ilzammulkhaq85@gmail.com>
2024-07-30 23:42:05 +0700

    feat: add basic splash screen

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIKERJA DINKES SUMEDANG</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+        rel="stylesheet">
+    @vite('resources/css/app.css')
+    <style>
+        * {
+            font-family: "Plus Jakarta Sans", sans-serif;
+            font-optical-sizing: auto;
+        }
+    </style>
+</head>
+
+<body>
+
+    @yield('content')
+
+</body>
+
+</html>
