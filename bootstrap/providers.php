853c7564b2dc9a42d4685b16537246d460e9f736
ilzam <ilzammulkhaq85@gmail.com>
2024-08-02 11:03:20 +0700

    feat: handover to aruva

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ContractServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 ];
